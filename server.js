03e5cba0afd1b8d13f1db839b931435757756eac
Puneet Lamba <pslamba@gmail.com>
2017-06-18 20:41:53 -0400

    v0.0.4 - added node web server and build

---
+++ server.js
@@ -0,0 +1,27 @@
+//server for react-times
+var 
+  express,
+	http,
+	loginErrorMessage,
+	session,
+  port,
+	server,
+	sid,
+
+//link required node.js modules  
+express = require("express");	
+http = require("http");
+session = require("express-session");
+
+port = 7000;
+server = express();
+
+server.use(express.static(__dirname + "/"));
+server.use(session({
+	secret:'topsecret',
+	saveUninitialized:true,
+	resave:true
+}));
+
+http.createServer(server).listen(process.env.PORT || port);
+console.log("server is listening on port " + (process.env.PORT || port));
